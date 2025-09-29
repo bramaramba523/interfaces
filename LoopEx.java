df9cb18a4212afb658e240a4fb87825734756325
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 22:44:53 +0530

    Add files via upload

---
+++ LoopEx.java
@@ -0,0 +1,26 @@
+package NewPack;
+
+public class LoopEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int i=1;
+	  Scanner  obj=new Scanner(System.in);
+	  int choice;
+  
+   System.out.println("current time"+java.time.locationDateTime.now());
+}
+else if (choice==3)
+{
+	System.out.println("are u want to exit");
+	char confirm=obj.next().charAt(0); 
+	if confirm=='y' || confirm=='y'){
+		System.out.println("good bye");
+		break;}
+	else {
+		System.out.println("continuing the program");
+		
+		
+   
+   }
+	}
